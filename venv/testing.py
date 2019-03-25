6a867dc75304d687ad09c9c45dd74a5acf7c791c
ikavanagh <ikavanagh@luc.edu>
2019-03-24 23:48:26 -0500

    added third party api's for corpus assemblance

--- venv/testing.py
+++ venv/testing.py
@@ -1,8 +1,8 @@
 import requests
 import bs4
 import sys
+import urllib
 
-url = input("enter side you would like to visit")
 
 def get_request():
     req = requests.get(url)
@@ -15,6 +15,21 @@ def parse_data():
     data = bs4.BeautifulSoup(get_request(),'html.parser')
     print(data.title)
 
+def use_arxiv(search_type,max_results):
+    url = 'http://export.arxiv.org/api/query?search_query=all:'+search_type+'&start=0&max_results='+max_results
+    data = urllib.request.urlopen(url).read()
+    soup = bs4.BeautifulSoup(data,'html.parser')
+    print(soup.prettify())
 
-get_request()
-parse_data()
+def use_caselaw():
+    url="https://api.case.law/v1/cases/435800/?full_case=true&format=html"
+    data = urllib.request.urlopen(url).read()
+    soup = bs4.BeautifulSoup(data,'html.parser')
+    sentence = soup.find_all('p',{'class':'summary'})
+    print(sentence);
+
+
+def use_cross_ref():
+    url = ""
+
+use_arxiv("molecule",'1')
